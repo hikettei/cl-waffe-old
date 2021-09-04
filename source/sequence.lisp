a4a2a8110e6913cf1eb572e0f97ae05d040b652f
hikettei <hikettei@gmail.com>
2021-09-04 21:54:32 +0900

    sequence

--- source/sequence.lisp
+++ source/sequence.lisp
@@ -6,16 +6,20 @@
 
 (defmacro defsequence (sequence-name as initargs &rest layers)
   `(defclass ,sequence-name (SequenceLayer)
-     ((self :accessor self
+     ((self :accessor seqself
 	    :initarg :self
 	    :initform ',as)
-      (initargs :accessor initargs
+      (initargs :accessor seqargs
 		:initarg :initargs
 		:initform ',initargs)
-      ,@(map 'list #'(lambda (layer) `(,(car layer) :initform ,(second layer)
+      (layers :accessor seqlayers
+	      :initargs :layers
+	      :initform ,(map 'list #'(lambda (layer) (car layer))
+			       layers))
+      ,@(map 'list #'(lambda (layer) `(,(car layer) :initform (quote ,(second layer))
 						    :initarg ,(intern (symbol-name
 								      (car layer))
-								     "KEYWORD"))) layers))))
-								      
+								      "KEYWORD")))
+	     layers))))
 
-
+(defmacro init-sequence (sequence-name &rest args)
