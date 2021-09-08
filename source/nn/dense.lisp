f225b196dc8085cc2808b0caada35b9e58c8f834
hikettei <hikettei@gmail.com>
2021-09-08 18:51:27 +0900

    layer

--- source/nn/dense.lisp
+++ source/nn/dense.lisp
@@ -1,5 +1,7 @@
 
 (in-package #:cl-waffe.nn)
 
-(deflayer Dense (w b) (x) model
+(deflayer Dense ((w (make-array w))
+		 (b (make-array b))) (x) model
   (+ w b x))
+
--- source/sequence.lisp
+++ source/sequence.lisp
@@ -3,11 +3,12 @@
 
 
 (defmacro symbolk (symbol) `(intern (symbol-name ,symbol) "KEYWORD"))
+(defmacro pushs (obj place) `(concatenate 'list (list ,obj) ,place))
 
 (defclass SequenceLayer ()
-  ((self :accessor seqself)
+  ((self    :accessor seqself)
    (initarg :accessor seqargs)
-   (layers :accessor seqlayers)))
+   (layers  :accessor seqlayers)))
 
 (defmacro defsequence (sequence-name as initargs &rest layers)
   `(progn
@@ -26,7 +27,7 @@
 	 (layers :accessor seqlayers
 		 :initargs :layers
 		 :initform ',(map 'list #'(lambda (layer) (car layer))
-			       layers))
+			       layers)) ; init layer sitai
 	 ,@(map 'list #'(lambda (layer) `(,(car layer) :initform (quote ,(second layer))
 						       :initarg ,(symbolk (car layer))))
 		layers)))))
@@ -35,17 +36,33 @@
   `(progn
      (defclass ,layer-name ()
        ((params :initarg :params
-		:initform ,params)
-	,@(map 'list #'(lambda (x) (list x :initarg (symbolk x)
+		:initform ',params)
+	(param-args :initarg :param-args
+		    :initform ',(map 'list
+				     #'(lambda (x) (symbol-name (car x))) params))
+	,@(map 'list #'(lambda (x) (list (car x)
+					   :initarg (symbolk (car x))
 					   :initform nil)) params)))
      (defmethod ,layer-name (,model-as ,@args)
-       (with-slots ,params ,model-as
+       (with-slots ,(map 'list #'(lambda (x) (car x)) params) ,model-as
 	 ,@body))))
 
 (defmacro initlayer (layer-name &rest args)
-  `(let ((model (make-instance ',layer-name)))
-     
-     ))
+  (let* ((model (make-instance layer-name))
+	 (params (slot-value model 'params))
+	 (pargs (slot-value model 'param-args)))
+	       `(let* (,@(loop for i from 0 to (1- (length params))
+			      :collect (list (car (nth i params))
+					     (nth i args)))
+		      ,@(map 'list #'(lambda (x)
+				       `(,(car x)
+					 ,(pushs 'progn (cdr x)))) params))
+		  (declare (special ,@(map 'list #'intern pargs)))
+		  (dotimes (i (length ',params))
+		    (setf (slot-value ,model (intern (nth i ',pargs)))
+			  (or (eval (eval (intern (nth i ',pargs))))
+			      (nth i ',args))))
+		     ,model)))
 
 (defmacro calllayer (model layer-name &rest args)
   `(let ((input-layer 1))
